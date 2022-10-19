d0e1a90c85e4eace144d66b9de111187ba78e138
GabelGwlhs <113708877+GabelGwlhs@users.noreply.github.com>
2022-10-19 09:36:49 -0700

    changed it to right code

--- RectangleTester.java
+++ RectangleTester.java
@@ -5,15 +5,17 @@ public class RectangleTester
         // Can call static methods
         // WITHOUT creating an object
         // Classname.method();
-        
+        Rectangle.rectEquations();
         
         // Cannot call non-static methods without
         // an object
         
         // WRONG
         //int area = Rectangle.area();
-        
+    
         // RIGHT
-        
+        Rectangle desk = new Rectangle(3,6);
+        int area = desk.area();
+        System.out.println("the area of my desk is "+area);
     }
 }
